d2f5186dabcbdbfdf557113eb134dc3735140515
CharlsYao2020 <yaochaozhi@sina.cn>
2020-05-17 15:18:22 +0800

    HelloWorld!!!

---
+++ src/com/charles/Blog.java
@@ -0,0 +1,42 @@
+package com.blog;
+
+import java.util.Date;
+
+public class Blog {
+    private int id;
+    private String title;
+    private String contents;
+    private Date createdtime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getCreatedtime() {
+        return createdtime;
+    }
+
+    public void setCreatedtime(Date createdtime) {
+        this.createdtime = createdtime;
+    }
+}
